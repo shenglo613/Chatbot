9ebd8b0123fe50bc24c5f2e31c688bc6816693c9
Sheng Lo <shenglo1@Shengs-MacBook-Pro.local>
2019-05-31 16:28:35 -0700

    Creating the Encoder RNN Layer

--- chatbot.py
+++ chatbot.py
@@ -152,5 +152,16 @@ def preprocess_targets(targets, word_to_int, batch_size):
     left_side = tf.fill([batch_size, 1], word_to_int['<SOS>'])
     right_side = tf.strided_slice(targets, [0, 0], [batch_size, -1], [1, 1])
     preprocessed_targets = tf.concat([left_side, right_side], 1)
+    return preprocessed_targets
     
-    
+# Creating the Encoder RNN Layer
+def encoder_rnn_layer(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
+    lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
+    lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob=keep_prob)
+    encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_dropout] * num_layers)
+    _, encoder_state = tf.nn.bidirectional_dynamic_rnn(cell_fw=encoder_cell,
+                                                       cell_bw=encoder_cell,
+                                                       sequence_length=sequence_length,
+                                                       inputs=rnn_inputs,
+                                                       dtype=tf.float32)
+    return encoder_state
