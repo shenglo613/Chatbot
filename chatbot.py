dd1545638c1d6716ef0bf1b7cfb7a253caaed718
Sheng Lo <shenglo1@Shengs-MBP.socal.rr.com>
2019-05-31 01:31:22 -0700

    Creating a list of all of the conversations

--- chatbot.py
+++ chatbot.py
@@ -3,7 +3,20 @@ import numpy as np
 import tensorflow as tf
 import re
 import time
-import os
 
+# Importing the datasets
 lines = open('/Users/shenglo1/Documents/chatbot/movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
 conversations = open('/Users/shenglo1/Documents/chatbot/movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+
+# Creating a dictionary that maps each line to its id
+id_to_line = {}
+for line in lines:
+    _line = line.split(' +++$+++ ')
+    if (len(_line) == 5) :
+        id_to_line[_line[0]] = _line[4]
+
+# Creating a list of all of the conversations
+conversations_ids = []
+for conversation in conversations:
+    _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+    conversations_ids.append(_conversation.split(','))
