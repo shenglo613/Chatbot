6784965ca421a78ed4e638753cab8eada09ecf56
Sheng Lo <shenglo1@Shengs-MacBook-Pro.local>
2019-05-31 13:24:56 -0700

    Doing a first cleaning of the text

--- chatbot.py
+++ chatbot.py
@@ -28,3 +28,25 @@ for conversation in conversations_ids:
     for i in range(len(conversation) - 1):
         questions.append(id_to_line[conversation[i]])
         answers.append(id_to_line[conversation[i+1]])
+        
+# Doing a first cleaning of the text
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"here's", "here is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"aren't", "are not", text)
+    text = re.sub(r"isn't", "is not", text)
+    text = re.sub(r"con't", " cannot", text)
+    text = re.sub(r"""[-()+"=\!*/?;,.:']{}#$%^~_|""", "", text)
+    return text
