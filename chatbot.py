dcb2fe9bf1a8c9dc8bfe7d9732fe845e2bdf9121
Sheng Lo <shenglo1@Shengs-MacBook-Pro.local>
2019-05-31 17:26:31 -0700

    Decoding the training set

--- chatbot.py
+++ chatbot.py
@@ -165,3 +165,29 @@ def encoder_rnn_layer(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_leng
                                                        inputs=rnn_inputs,
                                                        dtype=tf.float32)
     return encoder_state
+
+# Decoding the training set
+def decode_training_set(encoder_state, decoder_cell, decoder_embadded_input, 
+                        sequence_length, decoding_scope, output_function,
+                        keep_prob, batch_size):
+    attention_states = tf.zeros([batch_size, 1, decoder_cell.output_size])
+    attention_keys, attention_values, attention_score_function, attention_construct_function = tf.contrib.seq2seq.prepare_attention(attention_states, attention_option= 'bahdanau', num_units=decoder_cell.output_size)
+    training_decoder_function = tf.contrib.seq2seq.attention_decoder_fn_train(encoder_state[0], attention_keys, attention_values, attention_score_function, attention_construct_function, name="attn_dec_train")
+    decoder_ouput, decoder_final_state, decoder_final_context_state = tf.contrib.seq2seq.dynamic_rnn_decoder(decoder_cell, training_decoder_function, decoder_embadded_input, sequence_length, scope=decoding_scope)
+    decoder_ouput_dropout = tf.nn.dropout(decoder_ouput, keep_prob)
+    return output_function(decoder_ouput_dropout)
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
